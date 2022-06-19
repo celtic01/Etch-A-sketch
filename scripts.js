80c4a22cfe91234797ec1acc28026cfdb2f699de
andreib <andrei.bortas@outlook.com>
2022-06-19 16:53:09 +0300

    Add title and style to the title

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const title = document.createElement('div');
+const body = document.querySelector('body');
+
+title.innerText = "Etch a sketch";
+
+title.classList.add('title');
+
+document.body.appendChild(title);
