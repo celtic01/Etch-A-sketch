e720929afb397cd72cacf66d44ae44eb9ae20709
andreib <andrei.bortas@outlook.com>
2022-06-22 20:38:36 +0300

    Refactor

--- scripts.js
+++ scripts.js
@@ -4,22 +4,13 @@ const title = document.createElement('div');
 const buttons = document.querySelector('.buttons');
 const clear = document.createElement('button');
 
+//create grid
 createGrid(16);
 
-const allCells = container.querySelectorAll('.cell');
-allCells.forEach(cell => {cell.addEventListener('mouseover', (event) =>{event.target.style.backgroundColor = 'black';})})
+createTitle();
+createClearButton();
 
-body.style.backgroundColor = '#ffebb3';
-title.innerText = 'Etch a sketch!';
-title.classList.add('title');
-body.appendChild(title);
 
-clear.innerText = 'Clear';
-clear.classList.add('button-paper');
-
-clear.addEventListener('click', () => allCells.forEach(cell => cell.style.backgroundColor = 'white'))
-
-buttons.appendChild(clear);
 
 
 function createGrid(n) {
@@ -28,6 +19,7 @@ function createGrid(n) {
 
     newRow.setAttribute('style', `grid-column-start:${i}; grid-column-end:${i + 1};`);
     newRow.classList.add('newRow');
+
     for (let j = 0; j < n; j++) {
       let cell = document.createElement('div');
       cell.classList.add('cell');
@@ -35,6 +27,24 @@ function createGrid(n) {
       container.appendChild(newRow);
     }
   }
+  const allCells = container.querySelectorAll('.cell');
+  allCells.forEach(cell => { cell.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = 'black'; }) })
+
+}
+
+function createTitle()
+{
+  title.innerText = 'Etch a sketch!';
+title.classList.add('title');
+body.appendChild(title);
+}
+
+function createClearButton(){
+  const allCells = container.querySelectorAll('.cell');
+  clear.innerText = 'Clear';
+clear.classList.add('button-paper');
+clear.addEventListener('click', () => allCells.forEach(cell => cell.style.backgroundColor = 'white'))
+buttons.appendChild(clear);
 }
 
 
