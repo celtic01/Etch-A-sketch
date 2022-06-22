69a315efd8a95cbc7e8bf4d623741367eacbe199
andreib <andrei.bortas@outlook.com>
2022-06-22 20:01:05 +0300

    Add draw functionality

--- scripts.js
+++ scripts.js
@@ -9,36 +9,44 @@ title.innerText = 'Etch a sketch!';
 
 title.classList.add('title');
 
-
 createGrid(16);
 
 body.appendChild(title);
 
+const allCells = container.querySelectorAll('.cell');
 
+console.log(allCells);
 
+allCells.forEach(cell => {cell.addEventListener('mouseover', (event) =>{event.target.style.backgroundColor = 'black';})})
 
+  
 
-
-
-
-
-
-function createGrid(n)
+function changeColor()
 {
-for (let i = 0; i < n; i++) {
-  let newRow = document.createElement('div');
-  newRow.setAttribute('style', `grid-column-start:${i}; grid-column-end:${i + 1};`);
+  this.setAttribute('style','background-color: black')
+}
 
-  newRow.classList.add('newRow');
+function createGrid(n) {
+  for (let i = 0; i < n; i++) {
+    let newRow = document.createElement('div');
 
-  for (let j = 0; j < n; j++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    newRow.appendChild(cell);
-    container.appendChild(newRow);
+    newRow.setAttribute('style', `grid-column-start:${i}; grid-column-end:${i + 1};`);
+    newRow.classList.add('newRow');
+    for (let j = 0; j < n; j++) {
+      let cell = document.createElement('div');
+      cell.classList.add('cell');
+      newRow.appendChild(cell);
+      container.appendChild(newRow);
+    }
   }
 }
+
+function addEventListenerList(list, event, fn) {
+  for (var i = 0, len = list.length; i < len; i++) {
+      list[i].addEventListener(event, fn, false);
+  }
 }
+
 
 
 
