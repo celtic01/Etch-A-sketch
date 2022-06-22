f851a88ae9377d9830633615926b2d03c527d7b0
andreib <andrei.bortas@outlook.com>
2022-06-22 10:54:22 +0300

    Create a 16*16 grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
-const container = document.querySelector('container');
+const container = document.getElementById('grid-container');
 const title = document.createElement('div');
+
 
 body.style.backgroundColor = '#ffebb3';
 
@@ -8,7 +9,21 @@ title.innerText = 'Etch a sketch!';
 
 title.classList.add('title');
 
-document.body.appendChild(title);
+for (let i = 0; i < 16; i++) {
+  let newRow = document.createElement('div');
+  newRow.setAttribute('style',`grid-column-start:${i}; grid-column-end:${i+1};`);
+
+  for (let j = 0; j < 16; j++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    newRow.appendChild(cell);
+    container.appendChild(newRow);
+  }
+
+}
+
+body.appendChild(title);
+
 
 
 
