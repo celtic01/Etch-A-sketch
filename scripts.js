339c60d10f8d773cebedb96d1def46130d61b224
andreib <andrei.bortas@outlook.com>
2022-06-22 10:26:26 +0300

    Add title and container + style to them

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 const body = document.querySelector('body');
-
+const container = document.querySelector('container');
+const title = document.createElement('div');
 
 body.style.backgroundColor = '#ffebb3';
+
+title.innerText = 'Etch a sketch!';
+
+title.classList.add('title');
+
+document.body.appendChild(title);
+
+
+
