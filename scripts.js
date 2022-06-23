d69baf1ffb097adfc9198c5be6092e99e9f48492
andreib <andrei.bortas@outlook.com>
2022-06-23 13:35:21 +0300

    Fix perfomance issue on slider

--- scripts.js
+++ scripts.js
@@ -19,12 +19,12 @@ createTitle();
 function createGrid(n) {
   //if there are cells, delete them and create grid
   // if there are not cells, just create the grid
-  if(container.hasChildNodes())
-  {
-    container.innerHTML ='';
+  if (container.hasChildNodes()) {
+    container.innerHTML = '';
   }
-  let sizeOfCell = 500/n;
+  let sizeOfCell = 500 / n;
   for (let i = 0; i < n; i++) {
+
     let newRow = document.createElement('div');
 
     newRow.setAttribute('style', `grid-column-start:${i}; grid-column-end:${i + 1};`);
@@ -33,7 +33,7 @@ function createGrid(n) {
     for (let j = 0; j < n; j++) {
       let cell = document.createElement('div');
       cell.classList.add('cell');
-      cell.setAttribute('style',`width:${sizeOfCell}px;height:${sizeOfCell}px;`);
+      cell.setAttribute('style', `width:${sizeOfCell}px;height:${sizeOfCell}px;`);
       newRow.appendChild(cell);
       container.appendChild(newRow);
     }
@@ -43,32 +43,28 @@ function createGrid(n) {
 
 }
 
-function createTitle()
-{
+function createTitle() {
   title.innerText = 'Etch a sketch!';
-title.classList.add('title');
-body.appendChild(title);
+  title.classList.add('title');
+  body.appendChild(title);
 }
 
-function createClearButton()
-{
+function createClearButton() {
   const allCells = container.querySelectorAll('.cell');
   clear.classList.add('button-paper');
-  clear.addEventListener('click', () => {container.innerHTML = ''; createGrid(slider.value)});
-  clear.innerText='clear';
+  clear.addEventListener('click', () => { container.innerHTML = ''; createGrid(slider.value) });
+  clear.innerText = 'clear';
   buttons.appendChild(clear);
-  
+
 }
 
-function createAndUpdateSlider()
-{
+function createAndUpdateSlider() {
   createGrid(slider.value);
   output.innerHTML = slider.value + "x" + slider.value;
-
-slider.oninput = function () {
-  createGrid(this.value);
-  output.innerHTML = this.value + "x" + this.value;
-}
+  slider.onchange = function () {
+    createGrid(this.value);
+    output.innerHTML = this.value + "x" + this.value;
+  }
 }
 
 
