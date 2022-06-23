6b4a018835de3f0ea7aab25b00c1acbbc48d0c8d
andreib <andrei.bortas@outlook.com>
2022-06-23 14:03:42 +0300

    Add rgb button and black button

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
 const body = document.querySelector('body');
-const container = document.getElementById('grid-container');
-const title = document.createElement('div');
 const buttons = document.querySelector('.buttons');
+const title = document.createElement('div');
 const clear = document.createElement('button');
+const rgb = document.createElement('button');
+const black = document.createElement('button');
 let slider = document.getElementById('myRange');
 let output = document.getElementById('output');
+const container = document.getElementById('grid-container');
 
+let color = 'black';
+
+createRgbButton();
 
 createClearButton();
+
+createBlackButton();
 
 createAndUpdateSlider();
 
 createTitle();
-
-
-
 
 function createGrid(n) {
   //if there are cells, delete them and create grid
@@ -39,7 +43,13 @@ function createGrid(n) {
     }
   }
   const allCells = container.querySelectorAll('.cell');
-  allCells.forEach(cell => { cell.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = 'black'; }) })
+
+  if (color == 'random') {
+    allCells.forEach(cell => { cell.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = `${random_rgba()}`; }) })
+  }
+  else {
+    allCells.forEach(cell => { cell.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = `${color}`; }) })
+  }
 
 }
 
@@ -51,11 +61,25 @@ function createTitle() {
 
 function createClearButton() {
   const allCells = container.querySelectorAll('.cell');
-  clear.classList.add('button-paper');
+  clear.classList.add('button-6');
   clear.addEventListener('click', () => { container.innerHTML = ''; createGrid(slider.value) });
   clear.innerText = 'clear';
   buttons.appendChild(clear);
+}
+function createRgbButton() {
+  rgb.classList.add('button-6');
+  rgb.style.marginRight = '50px';
+  rgb.addEventListener('click', () => {color = 'random'; createGrid(slider.value) })
+  rgb.innerText = 'RGB';
+  buttons.appendChild(rgb);
+}
 
+function createBlackButton() {
+black.classList.add('button-6');
+black.style.marginLeft = '50px';
+black.addEventListener('click', () => {color = 'black'; createGrid(slider.value)})
+black.innerText = 'black';
+  buttons.appendChild(black);
 }
 
 function createAndUpdateSlider() {
@@ -65,6 +89,11 @@ function createAndUpdateSlider() {
     createGrid(this.value);
     output.innerHTML = this.value + "x" + this.value;
   }
+}
+
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
 
 
